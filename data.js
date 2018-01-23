f6627d61bd91ef3ffb96fae6d5e1b84880442520
AdrianaDillarza <itzel.dillarza@gmail.com>
2018-01-23 06:31:57 -0600

    Subiendo actualizaciones

---
+++ data.js
@@ -0,0 +1,101 @@
+var data = [{
+  'name': 'Toks Cuauhtémoc',
+  'type': 'Mexicana',
+  'address': 'Avenida Cuauhtémoc 64, Doctores, 06720 Ciudad de México, CDMX',
+  'price': '$200 p/p',
+  'phonenumber': '53829577',
+  'hour': '08:00 am - 22:00 pm',
+  'image': 'assets/images/toks.png',
+},
+
+{
+  'name': 'Azul Condesa',
+  'type': 'Mexicana',
+  'address': 'Av Nuevo León 68, Hipódromo Condesa, 06100 Ciudad de México, CDMX',
+  'price': '$350 p/p',
+  'phonenumber': '52870425',
+  'hour': '12:00 pm - 23:00 pm',
+  'image': 'assets/images/azul-condesa.jpg',
+},
+
+{
+  'name': 'Lardo',
+  'type': 'Italiana',
+  'address': 'Agustín Melgar 6, Condesa, 06140 Ciudad de México, CDMX',
+  'price': '$200 p/p',
+  'phonenumber': '55892457',
+  'hour': '11:00 am - 22:00 pm',
+  'image': 'assets/images/lardo.jpg',
+},
+
+{
+  'name': 'Los Moustaches',
+  'type': 'Internacional',
+  'address': 'Calle Río Sena 88, Cuauhtémoc, 06500 Ciudad de México, CDMX',
+  'price': '$250 p/p',
+  'phonenumber': '54268751',
+  'hour': '12:00 pm - 22:00 pm',
+  'image': 'assets/images/los moustaches.jpg',
+},
+
+{
+  'name': 'Chilis',
+  'type': 'Internacional',
+  'address': 'Londres 127, Juárez, 06600 Ciudad de México, CDMX',
+  'price': '$270 p/p',
+  'phonenumber': '5875951',
+  'hour': '12:00 pm - 23:00 pm',
+  'image': 'assets/images/chilis.jpg',
+},
+
+{
+  'name': 'Montejo',
+  'type': 'Mexicana',
+  'address': 'Av. Benjamín Franklin 261-A, Condesa, 06140 Ciudad de México, CDMX',
+  'price': '$200 p/p',
+  'phonenumber': '55619207',
+  'hour': '11:00 am - 23:00 pm',
+  'image': 'assets/images/montejo.jpg',
+},
+
+{
+  'name': 'La Cervecería del Barrio',
+  'type': 'Mexicana',
+  'address': 'Av. Vicente Suárez 38, Condesa, 06140 Ciudad de México, CDMX',
+  'price': '$300 p/p',
+  'phonenumber': '53870419',
+  'hour': '11:00 am - 00:00 am',
+  'image': 'assets/images/cerveceria.jpg',
+},
+
+{
+  'name': 'Pizza del Perro Negro',
+  'type': 'Italiana',
+  'address': 'Parque España 3, Roma Nte., 06700 Ciudad de México, CDMX',
+  'price': '$150 p/p',
+  'phonenumber': '72849632',
+  'hour': '11:00 am - 00:00 am',
+  'image': 'assets/images/perro-negro.jpeg',
+},
+
+{
+  'name': 'Contramar',
+  'type': 'Mariscos',
+  'address': 'Calle de Durango 200, Roma Nte., 06700 Ciudad de México, CDMX',
+  'price': '$250 p/p',
+  'phonenumber': '22874420',
+  'hour': '11:00 am - 23:00 pm',
+  'image': 'assets/images/contramar.jpg',
+},
+
+{
+  'name': 'La Bodeguita del Medio',
+  'type': 'Mexicana',
+  'address': 'Calle Cozumel 37, Cuauhtémoc, C. U. Benito Juárez, 06700 Ciudad de México, CDMX',
+  'price': '$250 p/p',
+  'phonenumber': '55896327',
+  'hour': '12:00 pm - 22:00 pm',
+  'image': 'assets/images/bodeguita.jpg',
+},
+
+]
---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+//  Vista Splash
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".content").fadeOut(1000);
+    },1000);
+});
+
+
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".content2").fadeIn(1000);
+    },1000);
+});
+
+
+//var $restaurants = $('.container-restaurants');
+//var $inputContainer = $('input');
+//var $restaurantContainer = $(".restaurants");
